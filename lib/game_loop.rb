8d19f7e940f6de255fc299afe15f836461725317
Wyatt <wyattwicks@gmail.com>
2021-02-11 18:16:27 -0700

    Add compare_guess change

--- lib/game_loop.rb
+++ lib/game_loop.rb
@@ -30,9 +30,9 @@ class GameLoop
     number_correct = 0
 
      @guess.each do |guess|
-       @answer.each do |answer|
+       @answer.uniq.each do |answer|
 
-          if guess_answer == true
+          if guess == answer
             number_correct += 1
           end
         end
@@ -43,33 +43,33 @@ class GameLoop
     p number_correct
   end
 
-  def answer_to_hash
-
-    hash_of_answers = {}
-    reds= []
-    greens = []
-    blues = []
-    yellows = []
-    @answer.find_all do |answer|
-      if answer == 'r'
-        reds << answer
-      elsif answer == 'g'
-        greens << answer
-      elsif answer == 'b'
-        blues << answer
-      elsif answer == 'y'
-        yellows << answer
-      end
-    end
-
-
-      hash_of_answers[reds[0]] = reds.length
-      hash_of_answers[greens[0]] = greens.length
-      hash_of_answers[blues[0]] = blues.length
-      hash_of_answers[yellows[0]] = yellows.length
-      p hash_of_answers
-  end
+  # def answer_to_hash
+  #
+  #   hash_of_answers = {}
+  #   reds= []
+  #   greens = []
+  #   blues = []
+  #   yellows = []
+  #   @answer.find_all do |answer|
+  #     if answer == 'r'
+  #       reds << answer
+  #     elsif answer == 'g'
+  #       greens << answer
+  #     elsif answer == 'b'
+  #       blues << answer
+  #     elsif answer == 'y'
+  #       yellows << answer
+  #     end
+  #   end
+  #
+  #
+  #     hash_of_answers[reds[0]] = reds.length
+  #     hash_of_answers[greens[0]] = greens.length
+  #     hash_of_answers[blues[0]] = blues.length
+  #     hash_of_answers[yellows[0]] = yellows.length
+  #     p hash_of_answers
+  # end
 
 end
-game = GameLoop.new.answer_to_hash
+game = GameLoop.new.user_guess
 #require "pry";binding.pry
